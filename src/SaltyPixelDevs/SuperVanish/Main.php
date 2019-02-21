2d34ac2c709cef2551e9cd48c6a2c0bae5f4a234
AlexPads <37447608+AlexPads@users.noreply.github.com>
2019-02-20 23:04:48 -0500

    Updates with Working Code!
    without sending to newly joined players though. :(

--- src/SaltyPixelDevs/SuperVanish/Main.php
+++ src/SaltyPixelDevs/SuperVanish/Main.php
@@ -4,16 +4,19 @@ declare(strict_types=1);
 
 namespace SaltyPixelDevs\SuperVanish;
 
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\entity\Entity;
 use pocketmine\event\Listener;
-use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\player\PlayerChatEvent;
 use pocketmine\network\mcpe\protocol\PlayerListPacket;
 use pocketmine\network\mcpe\protocol\types\PlayerListEntry;
-use pocketmine\plugin\PluginBase;
-use pocketmine\command\CommandSender;
-use pocketmine\command\Command;
 use pocketmine\Player;
-use pocketmine\entity\Entity;
+use pocketmine\plugin\PluginBase;
 use pocketmine\scheduler\PluginTask;
+use pocketmine\utils\TextFormat;
+use SaltyPixelDevs\SuperVanish\commands\sudo;
+use SaltyPixelDevs\SuperVanish\Task\Join;
 
 class Main extends PluginBase implements Listener{
 
@@ -21,8 +24,7 @@ class Main extends PluginBase implements Listener{
 
     public function onEnable() : void{
     }
-
-
+    
 	public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool{
 		switch($command->getName()){
 			case "sv":
@@ -36,24 +38,32 @@ class Main extends PluginBase implements Listener{
             case "fl":
                 $this->fl($sender);
                 return true;
+            case "sudo":
+                $this->sudo($sender, $args);
+                return true;
 			default:
 				return false;
 		}
 	}
+
     public function Vanish(Player $player){
         if ($player->isOp()) {
             $player->setDataFlag(Entity::DATA_FLAGS, Entity::DATA_FLAG_INVISIBLE, true);
             $player->setNameTagVisible(false);
             $player->sendPopup("§aYou are now Invisible to OtherPlayers!");
+            $name = $player->getName();
+            $this->getServer()->broadcastMessage("§e$name left the game");
             $entry = new PlayerListEntry();
             $entry->uuid = $player->getUniqueId();
 
             $pk = new PlayerListPacket();
             $pk->entries[] = $entry;
             $pk->type = PlayerListPacket::TYPE_REMOVE;
-            foreach ($this->getServer()->getOnlinePlayers() as $players) {
+
+            foreach($this->getServer()->getOnlinePlayers() as $players){
                 $players->sendDataPacket($pk);
             }
+
         }else{
             $player->sendMessage("You Have to be OP to use this Command!");
         }
@@ -62,8 +72,21 @@ class Main extends PluginBase implements Listener{
         if ($player->isOp()) {
             $player->setDataFlag(Entity::DATA_FLAGS, Entity::DATA_FLAG_INVISIBLE, false);
             $player->setNameTagVisible(true);
-            $player = $player->getName();
-            $this->getServer()->broadcastMessage("$player joined the game");
+            $name = $player->getName();
+            $this->getServer()->broadcastMessage("$name joined the game");
+            $entry = new PlayerListEntry();
+            $entry->uuid = $player->getUniqueId();
+            $entry->entityUniqueId = $player->getId();
+            $entry->xboxUserId = $player->getXuid();
+            $entry->username = $player->getName();
+            $entry->skin = $player->getSkin();
+            $pk = new PlayerListPacket();
+            $pk->entries[] = $entry;
+            $pk->type = PlayerListPacket::TYPE_ADD;
+            foreach($this->getServer()->getOnlinePlayers() as $players){
+
+                $players->sendDataPacket($pk);
+            }
         }else{
             $player->sendMessage("You have to be OP to use this command!");
         }
@@ -78,4 +101,27 @@ class Main extends PluginBase implements Listener{
             $player->sendMessage("You have to be OP to use this command!");
         }
     }
+    public function sudo(Player $player, $args){
+        if ($player->isOp()) {
+            if (isset($args[0]) && isset($args[1])) {
+                $player = $this->getServer()->getPlayerExact($args[0]);
+                if (strpos($args[1], ":") !== false) {
+                    $list = explode(":", $args[1]);
+                    if ($list[0] == "c") {
+                        $player->sendMessage(TextFormat::GREEN . "Sending message as " . $player->getName());
+                        $this->getServer()->getPluginManager()->callEvent($ev = new PlayerChatEvent($player, $list[1]));
+                        if (!$ev->isCancelled()) {
+                            $this->getServer()->broadcastMessage($this->getServer()->getLanguage()->translateString($ev->getFormat(), [$ev->getPlayer()->getDisplayName(), $ev->getMessage()]), $ev->getRecipients());
+                        }
+                    } else {
+                        $player->sendMessage("Please use the command correctly!");
+                    }
+                } else {
+                    $player->sendMessage(TextFormat::AQUA . "Command ran as " . $player->getName());
+                    $this->getServer()->dispatchCommand($player, $args[1]);
+                }
+            }
+        }
+    }
+
 }
